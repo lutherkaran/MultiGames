3dac023fd393375a6d7a74d3d050b4d342993124
Karan Luther <35023765+lutherkaran@users.noreply.github.com>
2023-11-02 13:43:39 -0400

    Added a winning condition
    There's one minor bug which needs to be fixed which happens when a player enters a correct character n number of times.

--- ThreadExample/HangmanGame.cpp
+++ ThreadExample/HangmanGame.cpp
@@ -5,20 +5,36 @@ void HangmanGame::Instruction()
 	std::cout << "In this hangman game, guess the letters of a fruit, Each wrong guess could kill your player." << std::endl;
 }
 
-void HangmanGame::DrawMan(std::string& message)
+void HangmanGame::DrawScene(std::string& message)
 {
-	std::cout << "LIVES: " << this->Lives;
-
-	std::cout << "\t\t\t\t____________________" << std::endl;
-	std::cout << "\t\t\t\t         |         |" << std::endl;
-	std::cout << "\t\t\t\t         O         |" << std::endl;
-	std::cout << "\t\t\t\t        / \\        |" << "           " << message << std::endl;
-	std::cout << "\t\t\t\t         |         |" << "          /" << std::endl;
-	std::cout << "\t\t\t\t        / \\        |         O" << std::endl;
-	std::cout << "\t\t\t\t     =========     |         / \\" << std::endl;
-	std::cout << "\t\t\t\t     |       |     |     0    |" << std::endl;
-	std::cout << "\t\t\t\t     |       |     |    /    / \\" << std::endl;
-	std::cout << "\t\t\t\t#############################################" << std::endl << std::endl;
+	std::cout << "LIVES: " << this->Lives << std::endl;
+	std::cout << "FRUIT TO GUESS IS: " << HiddenFruit << std::endl;
+	if (Lives > 0) {
+		std::cout << "\t\t\t\t____________________" << std::endl;
+		std::cout << "\t\t\t\t         |         |" << std::endl;
+		std::cout << "\t\t\t\t         O         |" << std::endl;
+		std::cout << "\t\t\t\t        / \\        |" << "           " << message << std::endl;
+		std::cout << "\t\t\t\t         |         |" << "          /" << std::endl;
+		std::cout << "\t\t\t\t        / \\        |         O" << std::endl;
+		std::cout << "\t\t\t\t     =========     |         / \\" << std::endl;
+		std::cout << "\t\t\t\t     |       |     |     0    |" << std::endl;
+		std::cout << "\t\t\t\t     |       |     |    /    / \\" << std::endl;
+		std::cout << "\t\t\t\t#############################################" << std::endl << std::endl;
+	}
+	else if (Lives == 0)
+	{
+		std::cout << "\t\t\t\t____________________" << std::endl;
+		std::cout << "\t\t\t\t         |         |" << std::endl;
+		std::cout << "\t\t\t\t         |         |" << std::endl;
+		std::cout << "\t\t\t\t        O)         |" << std::endl;
+		std::cout << "\t\t\t\t        / \\        |" << "           " << message << std::endl;
+		std::cout << "\t\t\t\t         |         |" << "          /" << std::endl;
+		std::cout << "\t\t\t\t     |  / \\  |      |         O" << std::endl;
+		std::cout << "\t\t\t\t     |       |     |         / \\" << std::endl;
+		std::cout << "\t\t\t\t     |       |     |     0    |" << std::endl;
+		std::cout << "\t\t\t\t     |       |     |      \\  / \\" << std::endl;
+		std::cout << "\t\t\t\t#############################################" << std::endl << std::endl;
+	}
 }
 
 void HangmanGame::Initializing()
@@ -30,13 +46,17 @@ void HangmanGame::Initializing()
 	Index = 0;
 	hidden = false;
 	DisplayMessageNumber = 0;
+	CorrectGuesses = 0;
 	int RandomFruitNumber = 0 + (std::rand() % GetTargetFruit().size());
-
-	DrawMan(GetMessage()[0]);
-	Instruction();
 
 	HiddenFruit = GetTargetFruit()[RandomFruitNumber];
 	GuessFruit = HiddenFruit;
+	length = GuessFruit.length();
+
+	HideWord(HiddenFruit);
+	DrawScene(GetMessage()[0]);
+
+	Instruction();
 }
 
 void HangmanGame::NewGame()
@@ -46,17 +66,16 @@ void HangmanGame::NewGame()
 	{
 		if (Lives > 0)
 		{
-			HideWord(HiddenFruit);
 			InputGame();
 			CheckGuess(DisplayMessageNumber);
 			system("CLS");
-			DrawMan(GetMessage()[DisplayMessageNumber]);
+			DrawScene(GetMessage()[DisplayMessageNumber]);
 
 		}
 		else if (Lives == 0)
 		{
 			system("CLS");
-			DrawMan(GetMessage()[4]);
+			DrawScene(GetMessage()[4]);
 			std::cout << "CORRECT ANSWER IS: " << GuessFruit << std::endl;
 			RestartGame();
 		}
@@ -74,40 +93,59 @@ void HangmanGame::HideWord(std::string& fruit)
 {
 	if (!hidden)
 	{
-		for (int i = 0; i < fruit.length(); i++)
+		for (int i = 0; i < length; i++)
 		{
 			fruit[i] = 'X';
 		}
 		hidden = true;
 	}
-	std::cout << "FRUIT TO GUESS: " << fruit << std::endl;
 }
 
-void HangmanGame::UnhideWord(char C, char Guess, int Index)
+void HangmanGame::UnhideWord(char c, int index, int& length)
 {
+	HiddenFruit[Index] = c;
+	bool characterRevealed = false;
+
 	if (hidden)
 	{
-		if (Guess == c)
+		for (int i = 0; i < length; i++) {
+			// Check if the guessed character is equal to the character at the current index
+			if (GuessFruit[i] == c)
+			{
+				HiddenFruit[i] = c;
+				CorrectGuesses++;
+				characterRevealed = true;
+			}
+		}
+	}
+
+	if (characterRevealed) {
+		if (CorrectGuesses == HiddenFruit.length())
 		{
-			HiddenFruit[Index] = c;
+			system("CLS");
+			DrawScene(GetMessage()[5]); // Display a winning message
+			std::cout << "THE CORRECT ANSWER IS: " << HiddenFruit << std::endl;
+			RestartGame();
 		}
 	}
 }
 
 void HangmanGame::CheckGuess(int& DisplayMessageNumber)
 {
-	for (int i = 0; i < GuessFruit.length(); i++)
+	for (int i = 0; i < length; i++)
 	{
 		if (Guess == GuessFruit[i])
 		{
 			srand(time(NULL));
 			c = GuessFruit[i];
 			Index = i;
-			UnhideWord(c, Guess, i);
+			UnhideWord(c, i, length);
 			DisplayMessageNumber = 1 + (std::rand() % 2);
+
 			break;
 		}
 	}
+
 	if (Guess != c)
 	{
 		Lives--;
--- ThreadExample/HangmanGame.h
+++ ThreadExample/HangmanGame.h
@@ -8,17 +8,19 @@ private:
 	std::vector<std::string> Fruits{ "APPLE", "BANANA", "MANGO", "CHERRY", "BERRY", "TOMATO", "GUAVA", "PEAR", "WATERMELON", "PAPAYA", "KIWI" };
 	std::vector<std::string> message{ "BEST OF LUCK..!!", "BRAVO..!!", "THAT'S CORRECT..!!" ,"WRONG ANSWER..!!","TIME TO DIE BITCH ..l..", "CONGRATULATIONS, YOU WON..!!" };
 
-	unsigned int Lives; 
+	unsigned int Lives;
 	char Guess; // Input
-	
+
 	std::string GuessFruit; // To Guess
 	std::string HiddenFruit;
-	
+
 	char c; // To track
 	int Index;
-	
+	int CorrectGuesses;
+
 	bool hidden;
 	int DisplayMessageNumber;
+	int length;
 
 public:
 
@@ -33,9 +35,9 @@ public:
 	virtual void NewGame() override;
 	virtual void InputGame() override;
 
-	void DrawMan(std::string& message);
+	void DrawScene(std::string& message);
 	void HideWord(std::string& fruit);
-	void UnhideWord(char C, char Guess, int Index);
+	void UnhideWord(char c, int index, int& n);
 	void CheckGuess(int& DisplayMessageNumber);
 	void Initializing();
 };
